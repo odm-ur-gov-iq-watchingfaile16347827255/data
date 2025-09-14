b79017a4569fda013191dcd2dd3907bab4f17b5d
odm-ur-gov-iq-watchingfaile16347827255 <odm.ur.gov.iq.watchingfail13652@gmail.com>
2025-09-14 15:04:27 +0300

    Modified by www.SourceFiles.app

---
+++ dm.ur.gov.iq+414 4/eye.gov.iq/chunk-mtpet4qz.js
@@ -0,0 +1,1 @@
+import{Y as e}from"./chunk-IFGGGOFT.js";var o=(()=>{class t{static \u0275fac=function(r){return new(r||t)};static \u0275prov=e({token:t,factory:t.\u0275fac})}return t})();export{o as a};
