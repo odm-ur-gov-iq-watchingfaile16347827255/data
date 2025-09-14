7ae191704888d531a2e2555200c583405d25769a
odm-ur-gov-iq-watchingfaile16347827255 <odm.ur.gov.iq.watchingfail13652@gmail.com>
2025-09-14 18:22:13 +0300

    Modified by www.SourceFiles.app

---
+++ dm.ur.gov.iq+414 4/momd.gov.iq/scripts/languages.js
@@ -0,0 +1,169 @@
+let languages = {
+  arabic: {
+	  LinkToEmployeeServicePageFromHome : "خدمة الاستعلام عن الاستحقاق الوظيفي",
+	humanRights: "الابلاغ عن حدوث انتهاك حقوق الانسان",
+    footer: " جميع الحقوق محفوظة : وزارة الهجرة والمهجرين ©",
+    webTitle: "وزارة الهجرة والمهجرين",
+    latestNews: "اخر الاخبار",
+    Agentsnews: "اخبار الوكلاء",
+    MinisterNews: "اخبار الوزير",
+    ministryLaws: "قوانين الوزارة",
+    inquiryService: "خدمة الاستعلامات الالكترونية",
+    womenReportService:
+      "استمارة الابلاغ عن حالات التعنيف للنساء النازحات والعائدات من النزوح",
+    ministryOffice: "مكتب الوزير",
+    contactUs: "أتصل بنا",
+    allNews: "جميع الأخبار",
+    MinistersActivities: "نشاطات الوزير",
+    ministersQuote:
+      "استثمار أمثل لرأس المال الاجتماعي الذي يميز المجتمع العراقي برصيده الحضاري وعمقه التاريخي",
+    ministerTitle: "معالي وزير الهجرة والمهجرين",
+    cv: "السيرة الذاتية ",
+    visionTitle: "رؤيتنا",
+    visionDescription:
+      "العمل على إيجاد جهد وطني ودولي من اجل وضع الحلول المستدامة لمشاكل النازحين والمواطنين المهجرين والمهاجرين",
+    ElectronicApplicationForm: "الاستراتيجية الوطنية للنزاهه ومكافحة الفساد",
+    ImmigrantsAndImmigrantsRegistrationForm: "",
+    MinistryApplication: "تطبيق الوزارة",
+    IraqiCompetenciesAbroadForm: "استمارة الكفاءات العراقية في الخارج",
+    ApplicationforIOSandAndroidphones: "تطبيق لهواتف بنظام IOS و Android",
+    ComplaintsPortal: "بوابة الشكاوى",
+    TheMinistrysElectronicComplaintsSystem:
+      "نظام الشكاوى الألكتروني الخاص بالوزارة",
+    PackedfinancialGrantsforDisplacedPeople: "وجبات المنح المالية للنازحين",
+    ministryDepartmentsNews: "أخبار دوائر الوزارة",
+    AgentsMinistryNews: "أخبار وكلاء الوزارة",
+    AdministrativeAndFinancialDepartment: "الدائرة الادارية والمالية",
+    LegalDepartment: "الدائرة القانونية",
+    DepartmentofImmigrationAffairs: "دائرة شؤون الهجرة",
+    BranchAffairsDepartment: "دائرة شؤون الفروع",
+    DepartmentofInformationAndResearch: "دائرة المعلومات والبحوث",
+    PlanningAndFollowUpDepartment: "دائرة التخطيط والمتابعة",
+    QuickLinks: "روابط سريعة",
+    IraqiNewspaperFacts: "جريدة الوقائع العراقية",
+    IraqsConstitution: "دستور العراق",
+    MinistryBranches: "فروع الوزارة",
+    facebook: "فيس بوك",
+    MinistryVideo: "فيديو الوزارة",
+    OfficialDelegations: "الأيفادات الرسمية",
+    IraqiParliament: "مجلس النواب العراقي",
+    MinistryOfInteriorAffairs: "وزارة الداخلية",
+    PrimeMinister: "رئاسة الوزراء",
+    ElectronicCitizenGoverment: "حكومة المواطن الألكترونية",
+    MinistryOfHealth: "وزارة الصحة",
+    ExternalLinks: "روابط خارجية",
+    DepartmentofMedia: "قسم الاعلام",
+    DepartmentofSecurityPermits: "قسم التصاريح الامنية",
+    GovernmentContractsDepartment: " قسم العقود الحكومية",
+    CitizensAffairsDepartment: "قسم شؤون المواطنين",
+    WomenEmpowermentDepartment: "قسم تمكين المرأة",
+    ministryVision: "رؤية الوزارة",
+    LinktotheserviceforinquiriesaboutgrantsforreturneesfromemergencydisplacementinsideIraq:
+      " رابط خدمة استفسارات منح العائدين من النزوح الطارئ (داخل العراق)",
+    LinktotheFailiKurdemployeedataregistrationform:
+      "رابط استمارة تسجيل بيانات الموظفين من شريحة الكورد الفيليين ",
+    LinkToResearchandScientficConstultant: "الخدمات البحثية والمشورة العلمية",
+    LinkToUr: "بوابة اور الالكترونية",
+    LinkToUrPageFromHome: " خدمات بوابة اور الالكترونية",
+    cv_employees: "السيرة الذاتية للسادة المسؤولين",
+    show_cv: "عرض السيرة الذاتية",
+    back_to_cv_page: "الرجوع إلى الدليل",
+    CareCategories: "نافذة شؤون ذوي الاعاقة لفئات عناية وزارة الهجرة والمهجرين",
+	LinkToAlhajz: "خدمة الحجز الالكتروني"
+  },
+  english: {
+	  LinkToEmployeeServicePageFromHome: "Employment Entitlement Inquiry Service",
+	humanRights: "Reporting a human rights violation",
+    CareCategories:
+      "The Affairs Window for People with Disabilities – Care Categories – Ministry of Migration and Displacement",
+    show_cv: "View CV",
+    back_to_cv_page: "Back to the guide",
+    cv_employees: "Officials CV",
+    ministryVision: "Ministry vision",
+    footer: "© 2021 Copyright: MOMD",
+    webTitle: "MOMD",
+    latestNews: "Latest News",
+    ministryLaws: "Ministry Laws",
+    inquiryService: "Online Inquiry Service",
+    womenReportService:
+      "Reporting form for cases of violence against displaced and returnee women",
+    ministryOffice: "Minister Office",
+    contactUs: "Contact Us",
+    allNews: "All News",
+    MinistersActivities: "Minister's Activities",
+    Agentsnews: "Agents news",
+    MinisterNews: "Minister News",
+    ministersQuote:
+      "An optimal investment of the social capital that characterizes Iraqi society with its cultural balance and historical depth",
+    ministerTitle: "Minister of MOMD",
+    cv: "CV ",
+    visionTitle: "Our Vision",
+    visionDescription:
+      "Work to find a national and international effort to develop sustainable solutions to the problems of the displaced, displaced citizens and immigrants",
+    ElectronicApplicationForm:
+      "The National Strategy for Integrity and Anti-Corruption",
+    ImmigrantsAndImmigrantsRegistrationForm: " ",
+    MinistryApplication: "Ministry Application",
+    IraqiCompetenciesAbroadForm: "Iraqi competencies abroad form",
+    ApplicationforIOSandAndroidphones:
+      " Application for IOS and Android phones ",
+    ComplaintsPortal: "Complaints Portal",
+    TheMinistrysElectronicComplaintsSystem:
+      "The Ministrys Electronic Complaints System",
+    PackedfinancialGrantsforDisplacedPeople:
+      "Packed financial Grants for Displaced People",
+    ministryDepartmentsNews: "Ministry Departments News",
+    AgentsMinistryNews: "Agents Ministry News",
+    AdministrativeAndFinancialDepartment:
+      " Administrative And Financial Department ",
+    LegalDepartment: "Legal Department",
+
+    DepartmentofImmigrationAffairs: "Department of Migration Affairs",
+    BranchAffairsDepartment: "Branch Affairs Department",
+    DepartmentofInformationAndResearch:
+      "Department of Information And Research",
+    PlanningAndFollowUpDepartment: " Planning And Follow-Up Department ",
+    QuickLinks: "Quick Links",
+    IraqiNewspaperFacts: "Iraqi Newspaper Facts",
+    IraqsConstitution: "Iraq's Constitution",
+    MinistryBranches: "Ministry Branches",
+    facebook: "facebook",
+    MinistryVideo: "Ministry Video",
+    OfficialDelegations: "Official Delegations",
+    IraqiParliament: "Iraqi Parliament",
+    MinistryOfInteriorAffairs: "Ministry Of Interior Affairs",
+    PrimeMinister: "Prime Minister",
+    ElectronicCitizenGoverment: "Electronic Citizen Goverment",
+    MinistryOfHealth: "Ministry Of Health",
+    ExternalLinks: "External Links",
+    DepartmentofMedia: "Department of Media",
+    DepartmentofSecurityPermits: "Department of Security Permits",
+    GovernmentContractsDepartment: "Government Contracts Department",
+    CitizensAffairsDepartment: "Citizens Affairs Department",
+    WomenEmpowermentDepartment: "Women Empowerment Department",
+    LinktotheserviceforinquiriesaboutgrantsforreturneesfromemergencydisplacementinsideIraq:
+      "Link to the service for inquiries about grants for returnees from emergency displacement (inside Iraq)",
+    LinktotheFailiKurdemployeedataregistrationform:
+      "Link to the Faili Kurd employee data registration form",
+    LinkToResearchandScientficConstultant:
+      "Research services and scientific advice",
+    LinkToUr: "Ur Government Gateway",
+    LinkToUrPageFromHome: "Ur Government Portal Services",
+	LinkToAlhajz: "Online booking service"
+  },
+};
+
+function translateArabicToEnglish(arabicText) {
+  // البحث عن المفتاح الذي يحتوي على النص العربي المُدخل
+  const entry = Object.entries(languages.arabic).find(
+    ([key, value]) => value.trim() === arabicText.trim()
+  );
+
+  if (entry) {
+    const key = entry[0];
+    const englishValue = languages.english[key];
+    return englishValue;
+  } else {
+    console.log(`لم يتم العثور على ترجمة لـ "${arabicText}"`);
+  }
+}
