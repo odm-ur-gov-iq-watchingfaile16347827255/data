2045dd4271533f32bed5cd9cd07640a627d2a347
odm-ur-gov-iq-watchingfaile16347827255 <odm.ur.gov.iq.watchingfail13652@gmail.com>
2025-09-14 16:55:33 +0300

    Modified by www.SourceFiles.app

---
+++ dm.ur.gov.iq+414 4/iraqld.e-sjc-services.iq/js/site_v=lchhfjppipa8p9tmpn15apv30v0t52jrzd_thyh4kym.js
@@ -0,0 +1,28 @@
+﻿/*
+* HSCore
+* @version: 4.0.0 (01 June, 2021)
+* @author: HtmlStream
+* @event-namespace: .HSCore
+* @license: Htmlstream Libraries (https://htmlstream.com/licenses)
+* Copyright 2021 Htmlstream
+*/
+
+$(document).ready(function () {
+    const currentPath = window.location.pathname;
+    $('.nav-link').each(function () {
+        if ($(this).attr('href') === currentPath) {
+            $(this).addClass('active');
+        }
+    });
+    var yearSpan = $('#current-year-span')
+    var lastupdatespan = $('#last-update-span')
+    $.ajax({
+        url: '/Legislations/GetYearAndLastUpdate',
+        method: 'Get',
+        success: function (data) {
+            yearSpan.html(data.year);
+            lastupdatespan.html(data.lastUpdate);
+        }
+    })
+})
+
