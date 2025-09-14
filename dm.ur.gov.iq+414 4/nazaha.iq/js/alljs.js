e2571a2b1c0f4bb5168915f60fdc500a98a4a27a
odm-ur-gov-iq-watchingfaile16347827255 <odm.ur.gov.iq.watchingfail13652@gmail.com>
2025-09-14 18:19:42 +0300

    Modified by www.SourceFiles.app

---
+++ dm.ur.gov.iq+414 4/nazaha.iq/js/alljs.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+function openNav() {
+  document.getElementById("mySidenav").style.width = "320px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.body.style.backgroundColor = "white";
+}
+
+
