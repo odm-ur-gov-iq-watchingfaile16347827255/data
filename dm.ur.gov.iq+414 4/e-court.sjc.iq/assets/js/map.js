3a679d19fc281bd2759c3bb1bcd10e7a6e97f45f
odm-ur-gov-iq-watchingfaile16347827255 <odm.ur.gov.iq.watchingfail13652@gmail.com>
2025-09-14 20:44:40 +0300

    Modified by www.SourceFiles.app

---
+++ dm.ur.gov.iq+414 4/e-court.sjc.iq/assets/js/map.js
@@ -0,0 +1,37 @@
+﻿var GoogleMapLink = "";
+function GetMap(lat, lang, label, googleMaplink) {
+    GoogleMapLink = googleMaplink;
+    var container = L.DomUtil.get('map');
+    // to skip error of the map container already intiliazed
+    if (container != null) {
+        container._leaflet_id = null;
+    }
+    var map = new L.map('map').setView([lat, lang], 13);
+      // to remove the ukreina flag and all attribution
+    map.removeControl(map.attributionControl);
+
+    var marker = new L.marker([lat, lang]).on('click', markerOnClick).addTo(map);
+    marker.bindTooltip(label, { permanent: true, offset: [0, 12] });
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19
+    }).addTo(map);
+}
+function markerOnClick(e) {
+ 
+    Swal.fire({
+        title: 'عرض الموقع على خرائط كوكل؟',
+        
+        showCancelButton: true,
+        confirmButtonText: 'تأكيد',
+        cancelButtonText: 'إلغاء',
+    }).then((result) => {
+       
+        if (result.isConfirmed) {
+            window.open(GoogleMapLink);
+
+        } else if (result.isDenied) {
+           
+        }
+    })
+   
+}
