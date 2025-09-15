287971fe6f985becbe675ac41c78193b33f8fd09
odm-ur-gov-iq-watchingfaile16347827255 <odm.ur.gov.iq.watchingfail13652@gmail.com>
2025-09-15 04:34:39 +0300

    Modified by www.SourceFiles.app

---
+++ dm.ur.gov.iq+414 4/www.google.com/maps/_/js/k=maps.lite.ar.bn5cwtad7z4.2019.o/ck=maps.lite.fkqyfhvw3hq.r.w.o/m=cohort_logging/am=kacama/rt=j/d=0/rs=act90oejn4_oaq0n0l74jgwz5yyjnukl_g/index.js
@@ -0,0 +1,8 @@
+"use strict";this._=this._||{};(function(_){var window=this;
+try{
+_.td("cohort_logging");
+var Gob=class{constructor(a){this.Fz=a}log(){this.Fz.lx(a=>{for(var b=0;b<_.Al(a,_.Wo,32);){var c=new Date(Date.now());c.setUTCHours(c.getUTCHours()-8);c.setUTCHours(0,0,0,0);c=c.getTime();var d=Number;var e=_.Xf(a,32,_.Wo,b);e=_.Zo(e,2);(c-d(e))/864E5>=60?_.lu(a,32,_.Wo,b):b++}for(const f of _.vo(a,_.Wo,32))b={L:_.F(f,1)},b!=null&&Number.isFinite(b.L)&&_.bp(a,b,!1)})}};_.ef(_.Tpa,new class{create(a){(new Gob(a.Oe)).log()}});
+_.Uj();
+}catch(e){_._DumpException(e)}
+}).call(this,this._);
+// Google Inc.
